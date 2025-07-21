85e10077a2366f9f3ff0de9bc43571f993e1ecee
chetan <chetan.agrawal@students.iiserpune.ac.in>
2025-07-21 14:05:58 +0530

    organised Analyzer

--- Geometry/Analyze/AnalyzeLightBSM.cc
+++ Geometry/Analyze/AnalyzeLightBSM.cc
@@ -53,16 +53,18 @@ void AnalyzeLightBSM::EventLoop(const char *detType,const char *inputFileList, c
   int Full_edep_evts = 0;
   int Comp2_evts = 0;
   int nDiffAngle = 0, nHits = -1, nRayl = 0;
+	
    
   for (Long64_t jentry=0; jentry<nentries;jentry++)
     {
+      int nHitComp = 0;
       double progress = 10.0 * jentry / (1.0 * nentries);
       int k = int (progress);
       if (k > decade)
 	cout << 10 * k << " %" << endl;
       decade = k;
 
-// ===============read this entry == == == == == == == == == == ==                                                                        
+      // ===============read this entry == == == == == == == == == == ==                                                                        
       Long64_t ientry = LoadTree(jentry);
 	
       if(Debug) cout<<"===load tree entry ==="<<jentry<<endl;
@@ -78,113 +80,95 @@ void AnalyzeLightBSM::EventLoop(const char *detType,const char *inputFileList, c
       h_Total_Edep-> Fill(Total_Edep);
 
       nHits = Hit_Time->size();
-      h_nHits->Fill(nHits);
 
-      for (int iHit =0; iHit < nHits; iHit++){
-	h_hitTime->Fill((*Hit_Time)[iHit]);
-	h_hitPosX->Fill((*Hit_PositionX)[iHit]);
-	h_hitPosY->Fill((*Hit_PositionY)[iHit]);
-	h_hitPosZ->Fill((*Hit_PositionZ)[iHit]);
-	  
+      //if (nHits>2){	
+      ROOT::Math::XYZVector v_comp, vin, vout, v1, v2;
+
+      //sorting the hits in the ascending order of time
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      vector<double> HitPosX_sort, HitPosY_sort, HitPosZ_sort, HitTime_sort, HitScatAng_sort, HitEta_sort, HitPol0_sort, HitPol1_sort, HitPol2_sort, HitEin_sort, HitEout_sort;
+      vector<double> HitPosX_cpy, HitPosY_cpy, HitPosZ_cpy, HitTime_cpy, HitScatAng_cpy, HitEta_cpy, HitPol0_cpy, HitPol1_cpy, HitPol2_cpy, HitEin_cpy, HitEout_cpy;
+
+      vector <int> HitProcId_sort, HitDetId_sort;
+      vector<int> HitProcId_cpy, HitDetId_cpy;
+	   
+      HitPosX_sort.clear();
+      HitPosY_sort.clear();
+      HitPosZ_sort.clear();
+      HitTime_sort.clear();
+      HitScatAng_sort.clear();
+      HitEta_sort.clear();
+      HitProcId_sort.clear();
+      HitPol0_sort.clear();
+      HitPol1_sort.clear();
+      HitPol2_sort.clear();	
+      HitEout_sort.clear();
+      HitEin_sort.clear();
+      HitDetId_sort.clear();
+	   
+      HitPosX_cpy = *Hit_PositionX;
+      HitPosY_cpy = *Hit_PositionY;
+      HitPosZ_cpy = *Hit_PositionZ;
+      HitTime_cpy = *Hit_Time;
+      HitScatAng_cpy = *Hit_ScatAngle;
+      HitEta_cpy = *Hit_Eta;
+      HitProcId_cpy = *Hit_ProcId;
+      HitPol0_cpy = *Hit_Pol0;
+      HitPol1_cpy = *Hit_Pol1;
+      HitPol2_cpy = *Hit_Pol2;
+      HitEout_cpy = *Hit_Eout;
+      HitEin_cpy = *Hit_Ein;
+      HitDetId_cpy = *Hit_DetId;
+	
+      for (int iHit=0 ; iHit< nHits; iHit++){                               	 
+	auto min_id_ptr = std::min_element(HitTime_cpy.begin(), HitTime_cpy.end());
+	size_t min_idx = std::distance(HitTime_cpy.begin(), min_id_ptr);
+
+	HitPosX_sort.push_back(HitPosX_cpy[min_idx]);
+	HitPosY_sort.push_back(HitPosY_cpy[min_idx]);
+	HitPosZ_sort.push_back(HitPosZ_cpy[min_idx]);
+	HitTime_sort.push_back(HitTime_cpy[min_idx]);
+	HitScatAng_sort.push_back(HitScatAng_cpy[min_idx]);
+	HitEta_sort.push_back(HitEta_cpy[min_idx]);
+	HitProcId_sort.push_back(HitProcId_cpy[min_idx]);
+	HitPol0_sort.push_back(HitPol0_cpy[min_idx]);
+	HitPol1_sort.push_back(HitPol1_cpy[min_idx]);
+	HitPol2_sort.push_back(HitPol2_cpy[min_idx]);
+	HitEout_sort.push_back(HitEout_cpy[min_idx]);
+	HitEin_sort.push_back(HitEin_cpy[min_idx]);
+	HitDetId_sort.push_back(HitDetId_cpy[min_idx]);
+	     
+	HitTime_cpy.erase(HitTime_cpy.begin() + min_idx);
+	HitPosX_cpy.erase(HitPosX_cpy.begin() + min_idx);
+	HitPosY_cpy.erase(HitPosY_cpy.begin() + min_idx);
+	HitPosZ_cpy.erase(HitPosZ_cpy.begin() + min_idx);
+	HitScatAng_cpy.erase(HitScatAng_cpy.begin() + min_idx);
+	HitEta_cpy.erase(HitEta_cpy.begin() + min_idx);
+	HitProcId_cpy.erase(HitProcId_cpy.begin() + min_idx);
+	HitPol0_cpy.erase(HitPol0_cpy.begin() + min_idx);
+	HitPol1_cpy.erase(HitPol1_cpy.begin() + min_idx);
+	HitPol2_cpy.erase(HitPol2_cpy.begin() + min_idx);
+	HitEout_cpy.erase(HitEout_cpy.begin() + min_idx);
+	HitEin_cpy.erase(HitEin_cpy.begin() + min_idx);
+	HitDetId_cpy.erase(HitDetId_cpy.begin() + min_idx);
       }
 
-      //if(Hit_Time->size() >1) {
-      //if(nHits >0){
-      //if (nHits==1){
-      //if (nHits==2){
-      //if (nHits>2){
+      //Sorting done
+      //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
-	ROOT::Math::XYZVector v_comp, vin, vout, v1, v2;
-
-	//sorting the hits in the ascending order of time
-	vector<double> HitPosX_sort, HitPosY_sort, HitPosZ_sort, HitTime_sort, HitScatAng_sort, HitEta_sort, HitPol0_sort, HitPol1_sort, HitPol2_sort;
-	vector<double> HitPosX_cpy, HitPosY_cpy, HitPosZ_cpy, HitTime_cpy, HitScatAng_cpy, HitEta_cpy, HitPol0_cpy, HitPol1_cpy, HitPol2_cpy;
-	vector <int> HitProcId_sort;
-	vector<int> HitProcId_cpy;
-	   
-	HitPosX_sort.clear();
-	HitPosY_sort.clear();
-	HitPosZ_sort.clear();
-	HitTime_sort.clear();
-	HitScatAng_sort.clear();
-	HitEta_sort.clear();
-	HitProcId_sort.clear();
-	HitPol0_sort.clear();
-	HitPol1_sort.clear();
-	HitPol2_sort.clear();
-	   
-	HitPosX_cpy.clear();
-	HitPosY_cpy.clear();
-	HitPosZ_cpy.clear();
-	HitTime_cpy.clear();
-	HitScatAng_cpy.clear();
-	HitEta_cpy.clear();
-	HitProcId_cpy.clear();
-	HitPol0_cpy.clear();
-	HitPol1_cpy.clear();
-	HitPol2_cpy.clear();
-	   
-	HitPosX_cpy = *Hit_PositionX;
-	HitPosY_cpy = *Hit_PositionY;
-	HitPosZ_cpy = *Hit_PositionZ;
-	HitTime_cpy = *Hit_Time;
-	HitScatAng_cpy = *Hit_ScatAngle;
-	HitEta_cpy = *Hit_Eta;
-	HitProcId_cpy = *Hit_ProcId;
-	HitPol0_cpy = *Hit_Pol0;
-	HitPol1_cpy = *Hit_Pol1;
-	HitPol2_cpy = *Hit_Pol2;
-	     
-	for (int iHit=0 ; iHit< nHits; iHit++){                               	 
-	  auto min_id_ptr = std::min_element(HitTime_cpy.begin(), HitTime_cpy.end());
-	  size_t min_idx = std::distance(HitTime_cpy.begin(), min_id_ptr);
-
-	  HitPosX_sort.push_back(HitPosX_cpy[min_idx]);
-	  HitPosY_sort.push_back(HitPosY_cpy[min_idx]);
-	  HitPosZ_sort.push_back(HitPosZ_cpy[min_idx]);
-	  HitTime_sort.push_back(HitTime_cpy[min_idx]);
-	  HitScatAng_sort.push_back(HitScatAng_cpy[min_idx]);
-	  HitEta_sort.push_back(HitEta_cpy[min_idx]);
-	  HitProcId_sort.push_back(HitProcId_cpy[min_idx]);
-	  HitPol0_sort.push_back(HitPol0_cpy[min_idx]);
-	  HitPol1_sort.push_back(HitPol1_cpy[min_idx]);
-	  HitPol2_sort.push_back(HitPol2_cpy[min_idx]);
-	     
-	  HitTime_cpy.erase(HitTime_cpy.begin() + min_idx);
-	  HitPosX_cpy.erase(HitPosX_cpy.begin() + min_idx);
-	  HitPosY_cpy.erase(HitPosY_cpy.begin() + min_idx);
-	  HitPosZ_cpy.erase(HitPosZ_cpy.begin() + min_idx);
-	  HitScatAng_cpy.erase(HitScatAng_cpy.begin() + min_idx);
-	  HitEta_cpy.erase(HitEta_cpy.begin() + min_idx);
-	  HitProcId_cpy.erase(HitProcId_cpy.begin() + min_idx);
-	  HitPol0_cpy.erase(HitPol0_cpy.begin() + min_idx);
-	  HitPol1_cpy.erase(HitPol1_cpy.begin() + min_idx);
-	  HitPol2_cpy.erase(HitPol2_cpy.begin() + min_idx);
-	}
-
-	int nHitComp = 0;
-	bool incrs_nHit = false;
+      bool incrs_nHit = false;
 	
-	if ( nHits>0 && HitProcId_sort[0] == 2) incrs_nHit = true;
+      if ( nHits>0 && HitProcId_sort[0] == 2) incrs_nHit = true;
 
-	for (int i = 0; i< HitTime_sort.size(); i++){	     
-	  if (incrs_nHit) nHitComp++;		  
-	}      
+      for (int i = 0; i< HitTime_sort.size(); i++){
+	if(incrs_nHit) nHitComp++;
+      }
 
+      h_nHits->Fill(nHitComp);
 
-
-	// //counting the events with atleast one rayleigh
-	// for (int i = 0; i< HitTime_sort.size(); i++){	     
-	//   //if ((*Hit_ProcId)[i] == 0) { nRayl++; break;}    //procId 0 for rayleigh
-	//   if (incrs_nHit) nHitComp++;		  
-	// }      
-
+      if(nHitComp >0){
 	//for (int iHit=0 ; iHit< nHits; iHit++){     //cannot calculate theta at the last hit
-	//if (nHits>=1){
-	//if (nHits==2){
-	
-	if(nHitComp ==2){
-	for (int iHit=0 ; iHit < 1; iHit++){     //considering only the first hit  
+	for (int iHit=0 ; iHit < 1; iHit++){     //considering only the first hit
 	  v_comp.SetXYZ(0,0,0);
 	  vin.SetXYZ(0,0,0);
 	  vout.SetXYZ(0,0,0);
@@ -194,9 +178,9 @@ void AnalyzeLightBSM::EventLoop(const char *detType,const char *inputFileList, c
 	  v_comp.SetXYZ(HitPosX_sort[iHit], HitPosY_sort[iHit], HitPosZ_sort[iHit]);
 
 	  if(iHit == 0) v1.SetXYZ(0,0,0);            //for first hit initial point will be gun postion
-	  //else v1.SetXYZ(HitPosX_sort[iHit-1], HitPosY_sort[iHit-1], HitPosZ_sort[iHit-1]);
+	  else v1.SetXYZ(HitPosX_sort[iHit-1], HitPosY_sort[iHit-1], HitPosZ_sort[iHit-1]);
 
-	  //v2.SetXYZ(HitPosX_sort[iHit+1], HitPosY_sort[iHit+1], HitPosZ_sort[iHit+1]);
+	  v2.SetXYZ(HitPosX_sort[iHit+1], HitPosY_sort[iHit+1], HitPosZ_sort[iHit+1]);
 
 	  vin = v_comp - v1;
 	  vout = v2 - v_comp;
@@ -205,13 +189,16 @@ void AnalyzeLightBSM::EventLoop(const char *detType,const char *inputFileList, c
 
 	  scat_theta = acos(vin.Dot(vout)/(vin.R()*vout.R()));
 
-	  
+	  double Time = HitTime_sort[iHit];
+	  double PosX = HitPosX_sort[iHit];
+	  double PosY = HitPosY_sort[iHit];
+	  double PosZ = HitPosZ_sort[iHit];	      
 	    
 	  double scat_theta_SD = HitScatAng_sort[iHit];
 	  double Ang_diff = scat_theta - scat_theta_SD;
 	  double scat_theta_deg = scat_theta*180.0 / TMath::Pi();
-	
-	  h_ScatAng_SDvsAna->Fill(scat_theta, scat_theta_SD);
+	  double scat_theta_SD_deg = scat_theta_SD*180.0 / TMath::Pi();	
+	  //h_ScatAng_SDvsAna->Fill(scat_theta, scat_theta_SD);
 
 	  double EtaPolScat = HitEta_sort[iHit];
 	  double EtaPolScat_deg = EtaPolScat*180.0 / TMath::Pi();
@@ -219,32 +206,79 @@ void AnalyzeLightBSM::EventLoop(const char *detType,const char *inputFileList, c
 
 	  float Pol0 = HitPol0_sort[iHit];
 	  float Pol1 = HitPol1_sort[iHit];
-	  float Pol2 = HitPol2_sort[iHit];	    
+	  float Pol2 = HitPol2_sort[iHit];
+
+	  float Eout = HitEout_sort[iHit];	  
+	  float Ein = HitEin_sort[iHit];
+
+	  int DetId = HitDetId_sort[iHit];	  
 	    
 	  h_diff_Ana_SD->Fill(Ang_diff);
+	  //}
+
+
+	  enum HitCat{all_inc =0, nHit_eq1 =1, nHit_gt1 =2, nHit_eq2 =3, nHit_gt2 =4};
+	  
+	  
+	  if (procId !=0 && procId != 1){       //excluding rayleigh && phot (not required exactly but kept of safety purpose)
+	    //if (procId !=0 && procId != 1 && (DetId/4000 >= 1)){       //excluding rayleigh && phot && BGO Hit
+	    h_ScatAng_SDvsAna->Fill(scat_theta, scat_theta_SD);
 	    
-	  if (procId !=0 && procId !=1){       //excluding rayleigh && phot
-	    Comp2_evts++;
-	    //h_theta->Fill(scat_theta_deg);
-	    h_theta->Fill(scat_theta_SD*180.0/TMath::Pi());
-	    h_eta->Fill(EtaPolScat_deg);	    
-	    h_theta_eta->Fill(EtaPolScat_deg, scat_theta_SD*180.0/TMath::Pi());
-	    //theta_eta->Fill(EtaPolScat_deg, scat_theta_deg);               );
-	    h_pol0->Fill(Pol0);
-	    h_pol1->Fill(Pol1);
-	    h_pol2->Fill(Pol2);		
-	    
-	    if(scat_theta_deg > 50 && scat_theta_deg < 100) h_compSigEta->Fill(EtaPolScat*180.0 / TMath::Pi());      //taking large values of theta to see more variation
-	  }	
+	    if (nHitComp>0) FillHistogram(all_inc, Time, PosX, PosY, PosZ, scat_theta_SD_deg, EtaPolScat_deg, Pol0, Pol1, Pol2, Ein, Eout);
+	    if (nHitComp==1) FillHistogram(nHit_eq1, Time, PosX, PosY, PosZ, scat_theta_SD_deg, EtaPolScat_deg, Pol0, Pol1, Pol2, Ein, Eout);
+	    if (nHitComp>1) FillHistogram(nHit_gt1, Time, PosX, PosY, PosZ, scat_theta_SD_deg, EtaPolScat_deg, Pol0, Pol1, Pol2, Ein, Eout);
+	    if (nHitComp==2) FillHistogram(nHit_eq2, Time, PosX, PosY, PosZ, scat_theta_SD_deg, EtaPolScat_deg, Pol0, Pol1, Pol2, Ein, Eout);
+	    if (nHitComp>2) FillHistogram(nHit_gt2, Time, PosX, PosY, PosZ, scat_theta_SD_deg, EtaPolScat_deg, Pol0, Pol1, Pol2, Ein, Eout);
+	  }     //procId condition end
+	  //}	
 
 	}   //end of iHit loop
-	//} //nhits>0 conditin brkt
       }       // nHit >1 condition brkt end			
     } //jentry loop end
 
   cout << "events with 2 or more comps: " << Comp2_evts << endl;
-  cout << "no. of events with atleast one rayleigh: " << nRayl << endl;
 }
+
+void AnalyzeLightBSM::FillHistogram(int cat, double time, double posX, double posY, double posZ, double theta_deg, double eta_deg, double pol0, double pol1, double pol2, double Ein, double Eout){
+  h_hitTime[cat]->Fill(time);
+  h_hitPosX[cat]->Fill(posX);
+  h_hitPosY[cat]->Fill(posY);
+  h_hitPosZ[cat]->Fill(posZ);
+
+  h_theta[cat]->Fill(theta_deg);
+
+  h_eta[cat]->Fill(eta_deg);	    
+  h_theta_eta[cat]->Fill(eta_deg, theta_deg);
+
+  h_pol0[cat]->Fill(pol0);
+  h_pol1[cat]->Fill(pol1);
+  h_pol2[cat]->Fill(pol2);
+
+  h_Ein[cat]->Fill(Ein);
+  h_Eout[cat]->Fill(Eout);
+  
+  h_theta_Eout[cat]->Fill(theta_deg, Eout);
+  
+  if(theta_deg > 50 && theta_deg < 100) h_compSigEta[cat]->Fill(eta_deg);    //taking large values of theta to see maximum variation
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //================================================================================================================================================================================================================
 
