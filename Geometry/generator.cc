d6465c0b55966b60fa527212ba1b0d31f3233f83
chetan <chetan.agrawal@students.iiserpune.ac.in>
2025-08-18 18:37:13 +0530

    Added 2nd pho with pol perp to pol1

--- Geometry/generator.cc
+++ Geometry/generator.cc
@@ -2,33 +2,13 @@
 MyPrimaryGenerator::MyPrimaryGenerator()
 {
   fParticleGun = new G4ParticleGun(1);
-  //gParticleGun = new G4ParticleGun(1);
-  //G4ThreeVector pos(0.*cm, 0.*cm, 0.*cm);
-
-  //G4ThreeVector mom(1.5, 2.5, 0.);
-  //G4ThreeVector mom(1.5, 2.5, 1.);
-
-  //G4ThreeVector mom(0, 1.5, 0);
-  //G4StokesVector pol(G4ThreeVector(-1., 0., 0.));
-  //pol = G4ThreeVector(0.,1.,0.);
-  
-  // fParticleGun->SetParticlePosition(pos);
-  // fParticleGun->SetParticleMomentumDirection(mom);
-  // fParticleGun->SetParticleMomentum(511*keV);
-
-  // G4ThreeVector gammaMom = fParticleGun->GetParticleMomentumDirection();
-  // G4ThreeVector pol = gammaMom.orthogonal().unit();
-  // pol = pol.rotate(2*CLHEP::pi * G4UniformRand(), gammaMom);     //randomizing the photon polarisation direction in the plane perp to gamma mom
-
-  // G4cout << "\nrandom number: " << G4UniformRand() << G4endl;
-  // G4cout << "\n\npolarsation direction: " << pol << G4endl;
-  // fParticleGun->SetParticlePolarization(pol);
+  gParticleGun = new G4ParticleGun(1);
 }
 
 MyPrimaryGenerator::~MyPrimaryGenerator()
 {
   delete fParticleGun;
-  //delete gParticleGun;
+  delete gParticleGun;
 }
 
 void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
@@ -47,22 +27,39 @@ void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
   
 
   fParticleGun->SetParticlePosition(pos);
+  gParticleGun->SetParticlePosition(pos);
+
   fParticleGun->SetParticleMomentumDirection(mom);
+  gParticleGun->SetParticleMomentumDirection(-1*mom);
+  
   fParticleGun->SetParticleMomentum(511*keV);
+  gParticleGun->SetParticleMomentum(511*keV);
+  
     
   G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
   G4String particleName="gamma";
   G4ParticleDefinition *particle = particleTable->FindParticle("gamma");
   
   fParticleGun->SetParticleDefinition(particle);
+  gParticleGun->SetParticleDefinition(particle);
 
   G4ThreeVector gammaMom = fParticleGun->GetParticleMomentumDirection();  
-  G4ThreeVector pol = gammaMom.orthogonal().unit();  
-  pol = pol.rotate(2*CLHEP::pi * G4UniformRand(), gammaMom);     //randomizing the photon polarisation direction in the plane perp to gamma mom  
-  fParticleGun->SetParticlePolarization(pol);
+  G4ThreeVector pol1 = gammaMom.orthogonal().unit();  
+  pol1 = pol1.rotate(2*CLHEP::pi * G4UniformRand(), gammaMom);     //randomizing the photon polarisation direction in the plane perp to gamma mom  
+  fParticleGun->SetParticlePolarization(pol1);
+
+  G4ThreeVector pol2 = pol1;
+  pol2 = pol2.rotate(CLHEP::pi / 2.0, gammaMom);
+  gParticleGun->SetParticlePolarization(pol2);
   
   fParticleGun->GeneratePrimaryVertex(anEvent);
+  gParticleGun->GeneratePrimaryVertex(anEvent);
   
-  G4cout << "\n\nMomentum direction of the incident photons: " << fParticleGun->GetParticleMomentumDirection () << G4endl;
-  G4cout << "polarisation vector (EF direction) of the incident photons: " << fParticleGun->GetParticlePolarization() << "\n\n";
+  G4cout << "\n\nMomentum direction of the 1st incident photons: " << fParticleGun->GetParticleMomentumDirection () << G4endl;
+  G4cout << "polarisation vector (EF direction) of the 1st incident photons: " << fParticleGun->GetParticlePolarization() << "\n\n";
+
+  G4cout << "Momentum direction of the 2nd incident photons: " << gParticleGun->GetParticleMomentumDirection () << G4endl;
+  G4cout << "polarisation vector (EF direction) of the 2nd incident photons: " << gParticleGun->GetParticlePolarization() << "\n\n";
+
+  G4cout << "Angle betn pol1 and pol2: " << acos(pol1.dot(pol2))*(180/CLHEP::pi) << " degs\n\n";
 }
