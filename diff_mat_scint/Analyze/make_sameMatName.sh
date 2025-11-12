dc99b9b6916fc4d9e0075c81251e0ef24d3289e9
chetan <chetan.agrawal@students.iiserpune.ac.in>
2025-11-12 11:06:50 +0530

    updated

--- diff_mat_scint/Analyze/make_sameMatName.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-Dir="$1"
-for f in $Dir/*.root; do mv "$f" "${f//_test/}"; done
--- diff_mat_scint/Analyze/txtClean.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm /home/chetan/Work/geant4/diff_mat_scint/Analyze/Rep_plots/Scintillation/Fits/txtFiles/*.txt
