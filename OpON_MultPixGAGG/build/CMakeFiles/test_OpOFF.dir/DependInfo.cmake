5ac127a6799f4b059b93bfbc95302187a8098722
chetan <chetan.agrawal@students.iiserpune.ac.in>
2026-01-03 15:17:12 +0530

    build files

---
+++ OpON_MultPixGAGG/build/CMakeFiles/test_OpOFF.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/chetan/Work/geant4/OpON_MultPixGAGG/CellHit.cc" "/home/chetan/Work/geant4/OpON_MultPixGAGG/build/CMakeFiles/test_OpOFF.dir/CellHit.cc.o"
+  "/home/chetan/Work/geant4/OpON_MultPixGAGG/CellSD.cc" "/home/chetan/Work/geant4/OpON_MultPixGAGG/build/CMakeFiles/test_OpOFF.dir/CellSD.cc.o"
+  "/home/chetan/Work/geant4/OpON_MultPixGAGG/action.cc" "/home/chetan/Work/geant4/OpON_MultPixGAGG/build/CMakeFiles/test_OpOFF.dir/action.cc.o"
+  "/home/chetan/Work/geant4/OpON_MultPixGAGG/construction.cc" "/home/chetan/Work/geant4/OpON_MultPixGAGG/build/CMakeFiles/test_OpOFF.dir/construction.cc.o"
+  "/home/chetan/Work/geant4/OpON_MultPixGAGG/event.cc" "/home/chetan/Work/geant4/OpON_MultPixGAGG/build/CMakeFiles/test_OpOFF.dir/event.cc.o"
+  "/home/chetan/Work/geant4/OpON_MultPixGAGG/generator.cc" "/home/chetan/Work/geant4/OpON_MultPixGAGG/build/CMakeFiles/test_OpOFF.dir/generator.cc.o"
+  "/home/chetan/Work/geant4/OpON_MultPixGAGG/physics.cc" "/home/chetan/Work/geant4/OpON_MultPixGAGG/build/CMakeFiles/test_OpOFF.dir/physics.cc.o"
+  "/home/chetan/Work/geant4/OpON_MultPixGAGG/run.cc" "/home/chetan/Work/geant4/OpON_MultPixGAGG/build/CMakeFiles/test_OpOFF.dir/run.cc.o"
+  "/home/chetan/Work/geant4/OpON_MultPixGAGG/stepping.cc" "/home/chetan/Work/geant4/OpON_MultPixGAGG/build/CMakeFiles/test_OpOFF.dir/stepping.cc.o"
+  "/home/chetan/Work/geant4/OpON_MultPixGAGG/test.cc" "/home/chetan/Work/geant4/OpON_MultPixGAGG/build/CMakeFiles/test_OpOFF.dir/test.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_RAYTRACERX"
+  "G4VIS_USE_TOOLSSG_QT_GLES"
+  "G4VIS_USE_TOOLSSG_QT_ZB"
+  "PTL_BUILD_DLL"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/chetan/root-6.26.06/installroot/include"
+  "/home/chetan/Work/geant4/geant4-v11.3.0-install/include/Geant4"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OpON_MultPixGAGG/build/CMakeFiles/test_OpOFF.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_OpOFF.dir/CellHit.cc.o"
+  "CMakeFiles/test_OpOFF.dir/CellSD.cc.o"
+  "CMakeFiles/test_OpOFF.dir/action.cc.o"
+  "CMakeFiles/test_OpOFF.dir/construction.cc.o"
+  "CMakeFiles/test_OpOFF.dir/event.cc.o"
+  "CMakeFiles/test_OpOFF.dir/generator.cc.o"
+  "CMakeFiles/test_OpOFF.dir/physics.cc.o"
+  "CMakeFiles/test_OpOFF.dir/run.cc.o"
+  "CMakeFiles/test_OpOFF.dir/stepping.cc.o"
+  "CMakeFiles/test_OpOFF.dir/test.cc.o"
+  "test_OpOFF"
+  "test_OpOFF.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_OpOFF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
